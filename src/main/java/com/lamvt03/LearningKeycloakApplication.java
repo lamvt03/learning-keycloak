811440d3aaafcc2eb4a1af3f8bff7f43e5b4ed14
Vo Truong Lam <votruonglam2109@gmail.com>
2024-04-13 15:39:24 +0700

    init

---
+++ src/main/java/com/lamvt03/LearningKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.lamvt03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningKeycloakApplication.class, args);
+	}
+
+}
